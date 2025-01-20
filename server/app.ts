83a871a230a52f2e5f25b386442beeb603109c9e
Ruslan Issapov <r_issapov@almaty.gamma.kz>
2025-01-20 13:21:41 +0500

    Настроил работу с db через orm prisma

--- server/app.ts
+++ server/app.ts
@@ -1,8 +1,21 @@
 import express, { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
 const app = express();
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+const prisma = new PrismaClient();
+
+async function getAllUsers() {
+  const users = await prisma.user.findMany();
+  return users;
+}
+
+app.get("/", async (req: Request, res: Response) => {
+  try {
+    const users = await getAllUsers(); // Ждем, пока промис не выполнится
+    res.json(users); // Отправляем пользователей в формате JSON
+  } catch (error) {
+    res.status(500).send("Error fetching users");
+  }
 });
